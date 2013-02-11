e4ad4fcde1e1d90726f32f87a67b7e7d5d58e877
Tim Perkins <tim@livingstaccato.com>
2013-02-10 23:19:09 -0800

    Added some comments, braces, and another !empty check.

--- pi.http_response_header.php
+++ pi.http_response_header.php
@@ -10,29 +10,38 @@ class Plugin_http_response_header extends Plugin {
 
   public function index()
   {
-    // hmm. there has got to be a better way.
+    // Hmm. There has got to be a better way.
     global $app;
 
-    $if_user_agent = $this->fetch_param('if_user_agent', null, 'is_string', false, false);
+    $if_user_agent = $this->fetch_param('if_user_agent', null, 'is_string',
+                                        false, false);
     $field = $this->fetch_param('field', null, 'is_string', false, false);
     $value = $this->fetch_param('value', null, 'is_string', false, false);
     $meta = $this->fetch_param('meta', false, false, true);
 
-    if (!empty($app) && !empty($if_user_agent))
+    // If $app and $if_user_agent aren't empty then set $user_agent to the
+    // USER_AGENT
+    if (!empty($app) && !empty($if_user_agent)) {
       $user_agent = $app->request()->getUserAgent();
-
-    if (!empty($if_user_agent) && (strpos($user_agent, $if_user_agent) === false))
-      return false;
-
-    if (!empty($field) && !empty($value)) {
-
-      if (!empty($app))
-        $app->response()->header($field, $value);
-
-      if ($meta)
-        return '<meta http-equiv="'.$field.'" content="'.$value.'" />';
-
     }
 
+    // If the value of $if_user_agent is found in $user_agent then return
+    // nothing.
+    if (!empty($if_user_agent) && !empty($user_agent) && 
+       (strpos($user_agent, $if_user_agent) === false)) {
+      return false;
+    }
+
+    // If the $field and $value aren't empty then set the header and
+    // return a meta tag if $meta is true.
+    if (!empty($field) && !empty($value)) {
+      if (!empty($app)) {
+        $app->response()->header($field, $value);
+      }
+
+      if ($meta) {
+        return '<meta http-equiv="'.$field.'" content="'.$value.'" />';
+      }
+    }
   }
 }
